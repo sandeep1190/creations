5bcc95d86f74fea0be7aeb44aa13d986ea4ffe28
Sandeep Dhiman <sandeep.dhiman@exate.com>
2025-01-16 11:14:20 +0530

    changes in site

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'creationnew' );
+define( 'DB_NAME', 'wp_creations' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'wp_user' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'Creation@wp@2025' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
